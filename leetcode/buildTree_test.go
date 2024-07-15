1adece9dfc5c5e8abe49448391400048d2f16687
wuzeyu <570143426@qq.com>
2024-07-15 23:38:28 +0800

    新增从前序遍历与中序遍历构建二叉树

---
+++ leetcode/buildTree_test.go
@@ -0,0 +1,11 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+
+}
--- leetcode/twosum_test.go
+++ leetcode/twosum_test.go
@@ -1,7 +1,9 @@
 package leetcode
 
+import "testing"
+
 func twoSum(nums []int, target int) []int {
-	var numMap map[int]int
+	var numMap = make(map[int]int)
 	for index1, num := range nums {
 		index2, ok := numMap[target-num]
 		if ok {
@@ -12,4 +14,11 @@ func twoSum(nums []int, target int) []int {
 	}
 
 	return nil
+}
+
+func Test_twoSum(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		got := twoSum([]int{2, 7, 11, 15}, 9)
+		t.Log(got)
+	})
 }
