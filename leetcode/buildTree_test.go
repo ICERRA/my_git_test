7730026d04d87eebb380afdc855f6461245ed466
wuzeyu <570143426@qq.com>
2024-07-24 23:29:27 +0800

    新增从前序遍历与中序遍历构建二叉树

--- leetcode/buildTree_test.go
+++ leetcode/buildTree_test.go
@@ -1,11 +1,84 @@
 package leetcode
 
+import (
+	"math"
+	"slices"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+// buildTree 前序中序构建二叉树
 func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
 
+	// 获取左子树的长度
+	leftTreeSize := slices.Index(inorder, preorder[0])
+	leftTree := buildTree(preorder[1:1+leftTreeSize], inorder[:leftTreeSize])
+	rightTree := buildTree(preorder[1+leftTreeSize:], inorder[1+leftTreeSize:])
+
+	var rootNode = &TreeNode{
+		Val:   preorder[0],
+		Left:  leftTree,
+		Right: rightTree,
+	}
+
+	return rootNode
 }
+
+// region 二叉树的最大深度
+
+// maxDepth 深度优先
+func maxDepth(root *TreeNode) int {
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	if root.Left == nil {
+		return maxDepth(root.Right) + 1
+	}
+
+	if root.Right == nil {
+		return maxDepth(root.Left) + 1
+	}
+
+	return int(math.Max(float64(maxDepth(root.Left)), float64(maxDepth(root.Right)))) + 1
+}
+
+// maxDepth1 广度优先
+func maxDepth1(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var (
+		deepth int
+		queue  = make([]*TreeNode, 0)
+	)
+
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		size := len(queue)
+		for size > 0 {
+			rootNode := queue[0]
+			queue = queue[1:]
+
+			if rootNode.Left != nil {
+				queue = append(queue, rootNode.Left)
+			}
+			if rootNode.Right != nil {
+				queue = append(queue, rootNode.Right)
+			}
+			size--
+		}
+		deepth++
+	}
+	return deepth
+}
+
+//endregion
